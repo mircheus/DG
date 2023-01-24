b5f2e64af57a2e94a9fdb2fe41e881a26c301aee
mircheus <mircheuss@yandex.ru>
2023-01-24 17:31:32 +0100

    start NinjaAudo.cs edit

--- Assets/Scripts/Sound/NinjaAudo.cs
+++ Assets/Scripts/Sound/NinjaAudo.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class NinjaAudo : MonoBehaviour
 {
     [SerializeField] private AudioClip _swordAttackSound;
